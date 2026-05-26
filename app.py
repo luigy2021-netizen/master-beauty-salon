fa5c27208ad4840f87ef8ee53585afc19a46ffc4
luigy2021-netizen <luigy2021@gmail.com>
2026-05-25 20:45:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ def get_sheet():
 
     return sheet
 
+    return sheet
+
 # 2. Encabezado principal
 st.image("https://raw.githubusercontent.com/luigy2021-netizen/diangello-legend/main/diangello.png" , width=200)
 st.title("Di'Angello Legend")
